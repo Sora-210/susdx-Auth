46a78ad09674b23d822cac6fc286ec16959d22e1
Sora210 <sokominto1231@gmail.com>
2022-06-30 16:04:30 +0000

    :sparkles: Read pem file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 //Import
 import { logger } from './logger'
 
+import { join } from 'path'
+import { readFile } from 'fs'
+
 import { Express } from 'express'
 
 const express = require('express')
@@ -8,6 +11,32 @@ const cors = require('cors')
 
 //Setting
 const PORT = process.env.PORT || 80
+const PUBLICKKEYPATH = process.env.PUBLICKEYPATH || join(__dirname, '..', 'keys', 'publicKey.pem')
+const PRIVATEKEYPATH = process.env.PUBLICKEYPATH || join(__dirname, '..', 'keys', 'privateKey.pem')
+
+//Read publicKey
+let PUBLIC_KEY:String
+readFile(PUBLICKKEYPATH, (e, data) => {
+    if (e) {
+        logger.error('PublicKeyError:')
+        logger.error(e)
+        process.exit(1)
+    }
+    PUBLIC_KEY = data.toString()
+    logger.info('Successed to read Public Key File')
+})
+
+//Read privateKey
+let PRIVATE_KEY:String
+readFile(PRIVATEKEYPATH, (e, data) => {
+    if (e) {
+        logger.error('PrivateKeyError:')
+        logger.error(e)
+        process.exit(1)
+    }
+    PRIVATE_KEY = data.toString()
+    logger.info('Successed to read Private Key File')
+})
 
 //Create express Instance
 const app:Express = express()
@@ -51,10 +80,7 @@ app.post('/refresh', (req, res) => {
 })
 
 app.get('/publickey', (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        details: 'No coding'
-    })
+    res.status(200).send(PUBLIC_KEY)
 })
 
 //Account
